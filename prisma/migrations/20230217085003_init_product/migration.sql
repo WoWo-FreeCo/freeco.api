7462d40d0b0f0f36a1304fa3e57e0cf1bd22a544
meta-crawler <williamlei0906@gmail.com>
2023-02-17 15:25:07 +0800

    feat(wowo): alter `Product` prisma schema
    add productAttribute (enum type) to Product table

--- prisma/migrations/20230217085003_init_product/migration.sql
+++ prisma/migrations/20230217085003_init_product/migration.sql
@@ -10,13 +10,14 @@ CREATE TABLE `ProductCategory` (
 
 -- CreateTable
 CREATE TABLE `Product` (
-    `id` VARCHAR(36) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
     `memberPrice` INTEGER NOT NULL,
     `vipPrice` INTEGER NOT NULL,
     `svipPrice` INTEGER NOT NULL,
-    `productCategoryId` INTEGER NOT NULL,
+    `attribute` ENUM('GENERAL', 'CODE_CHAIN') NOT NULL DEFAULT 'GENERAL',
+    `categoryId` INTEGER NULL,
     `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updatedAt` TIMESTAMP(0) NOT NULL,
 
@@ -24,4 +25,4 @@ CREATE TABLE `Product` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Product` ADD CONSTRAINT `Product_productCategoryId_fkey` FOREIGN KEY (`productCategoryId`) REFERENCES `ProductCategory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `ProductCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
